03c6e02d0e5472f6c32b929462f1938cf629cb4d
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 15:43:29 +0530

    Divisible by 7 but not multiple of 5

---
+++ source/repos/C#-Programs/C#-Programs/DivisibleBy7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    internal class DivisibleBy7
+    {
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 2000; i <= 3200; i++)
+        //    {
+        //        if(i % 7==0 && i % 5 != 0)
+        //        {
+        //            Console.Write(i + ", ");
+
+        //        }
+
+        //    }
+        //}
+    }
+}
