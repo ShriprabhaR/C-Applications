b62151738e0508b40efd213c4837f4ba83b1f7c3
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 16:05:09 +0530

    Execution time

---
+++ source/repos/C#-Programs/C#-Programs/ExecutionTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    internal class ExecutionTime
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            for (int i = 0; i < 1000000; i++)
+            {
+                double time = Math.Sqrt(i);
+            }
+
+            DateTime end = DateTime.Now;
+
+            TimeSpan executionTime = start - end;
+            Console.WriteLine("Execution Time: "+executionTime.TotalMilliseconds);
+        }
+
+    }
+}
