1361a721be628f24856082fa4f65d0c3f3f7423d
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 13:47:13 +0530

    Place name

---
+++ source/repos/C#-Programs/C#-Programs/PlaceToUpperLower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    internal class PlaceToUpperLower
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    Console.WriteLine("Enter a place name you would like to visit");
+        //    String place = Console.ReadLine();
+
+        //    string Uplace= place.ToUpper();
+        //    Console.WriteLine("Place in UpperCase: "+Uplace);
+
+        //    string Lplace = place.ToLower();
+        //    Console.WriteLine("Place in LowerCase: "+Lplace);
+        //}
+    }
+}
