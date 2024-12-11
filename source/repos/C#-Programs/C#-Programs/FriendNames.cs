96f75e9b995a127ffee7ca91df4c867183e8c025
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 13:31:49 +0530

    Friend's names

---
+++ source/repos/C#-Programs/C#-Programs/FriendNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C__Programs
+{
+    internal class FriendNames
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Friend's names");
+        //    List<string> list = new List<string>();
+        //    list.Add("Dhanya");
+        //    list.Add("Manya");
+        //    list.Add("Kiran");
+        //    list.Add("Roop");
+        //    list.Add("Pavan");
+        //    foreach (string friend in list)
+        //    {
+        //        Console.WriteLine(friend);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+    }
+}
