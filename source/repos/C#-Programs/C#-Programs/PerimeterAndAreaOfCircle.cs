5341c80e70241a95152ae10fe0be3339ed41812a
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 15:45:08 +0530

    finding perimeter and area of circle

---
+++ source/repos/C#-Programs/C#-Programs/PerimeterAndAreaOfCircle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    internal class PerimeterAndAreaOfCircle
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Enter the diameter of the circle: ");
+        //    double diameter = Convert.ToDouble(Console.ReadLine());
+
+        //    const double pi = Math.PI;
+        //    double radius = diameter / 2;
+
+        //    double perimeter = pi * diameter;
+
+        //    double area = pi* radius* radius;
+
+        //    Console.WriteLine("Perimeter of the circle: "+perimeter);
+        //    Console.WriteLine("Area of the circle: "+area);
+
+        //    Console.ReadLine();
+
+        //}
+    }
+}
