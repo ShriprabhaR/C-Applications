146b8fe55c88b23dd60b682a8de05962b4b70f1d
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 14:29:21 +0530

    Printing 10 times the given number

---
+++ source/repos/C#-Programs/C#-Programs/Print10TimeString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    //internal class Print10TimeString
+    //{
+
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Enter a number");
+    //        String a = Console.ReadLine();
+    //        for (int i = 1; i <= 10; i++)
+    //        {
+    //            Console.WriteLine(i+"->"+a);
+    //        }
+
+    //        Console.ReadLine();
+    //    }
+    //}
+}
