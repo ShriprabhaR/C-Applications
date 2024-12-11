ed735bf43d01f4a68d9df62696c019832875efec
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 15:48:32 +0530

    Average of random Numbers

---
+++ source/repos/C#-Programs/C#-Programs/AvgOfRandomNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    internal class AvgOfRandomNumbers
+    {
+        //static void Main(string[] args)
+        //{
+        //    Random random = new Random();
+        //    int sum = 0;
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        int newRand = random.Next(10, 40);
+        //        sum = sum + newRand;
+        //        Console.Write(sum+" ");
+        //    }
+        //    double avg  = (double)sum / 5;
+        //    Console.WriteLine("\nAverage of 5 numbers "+avg);
+        //}
+    }
+}
