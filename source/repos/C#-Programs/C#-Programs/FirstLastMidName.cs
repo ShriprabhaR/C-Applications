33b7cd28f720e0d7f86e3ba004c9df5379c20a85
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-11 14:50:52 +0530

    Length of name and printing the name

---
+++ source/repos/C#-Programs/C#-Programs/FirstLastMidName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Programs
+{
+    internal class FirstLastMidName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Full name");
+            String FullName = Console.ReadLine();
+           String[] parts = FullName.Split(' ');
+            int len = parts.Length;
+            Console.WriteLine("Number of parts in the name: "+len);
+           
+            foreach (String part in parts)
+            {
+                Console.Write(part+" ");
+            }
+        }
+    }
+}
